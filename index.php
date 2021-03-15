ce98bdf813bf2b27d6999e403c067d73a3b401e6
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-15 20:16:41 +0100

    topbar i varaijable

--- index.php
+++ index.php
@@ -5,11 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Advokat</title>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.css">
     <link rel="stylesheet" href="scss/style.css">
 </head>
 <body>
+
+    <!-- top bar -->
+    <section class="topBar">
+        <a href="" class="logo">Law <span>office</span></a>
+        <ul>
+            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+        </ul>
+    </section>
 
 </body>
 </html>
