378661b759ca2ed016b4efb3906a280191ddb64b
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-15 21:16:05 +0100

    header html

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             </ul>
         </nav>
         <hgroup>
-            <h2>Experience - Trust - Results</h2>
+            <h1>Experience - Trust - Results</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, ea. Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati? Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati?</p>
             <button>View more</button>
         </hgroup>
