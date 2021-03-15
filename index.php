4cf7eaf2f8e0c59165c82f2ca2d926699f7a0c0e
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-15 20:53:32 +0100

    header html

--- index.php
+++ index.php
@@ -11,13 +11,29 @@
 
     <!-- top bar -->
     <section class="topBar container">
-        <a href="" class="logo">Law <span>office</span></a>
+        <a href="" class="logo">Law <span>Office</span></a>
         <ul>
             <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
             <li><a href=""><i class="fab fa-twitter"></i></a></li>
             <li><a href=""><i class="fab fa-instagram"></i></a></li>
         </ul>
     </section>
+
+    <!-- mainHeader -->
+    <header class="mainHeader">
+        <nav class="container">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <hgroup>
+            <h2>Experience - Trust - Results</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, ea. Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati? Fugit ab nihil eveniet accusamus vel. Accusantium eos, voluptate. Quos recusandae voluptas adipisci ipsa officia, repellendus ea similique labore obcaecati?</p>
+        </hgroup>
+    </header>
 
 </body>
 </html>
